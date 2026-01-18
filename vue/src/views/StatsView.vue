0627fda891f49de69ef2b1754034d00276ef21e9
SebastianThomas <kontakt@sthomas.ch>
2026-01-18 18:25:03 +0100

    Fix lint for Routing

--- vue/src/views/StatsView.vue
+++ vue/src/views/StatsView.vue
@@ -144,7 +144,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, onBeforeUnmount, watch } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useApi } from '@/composables/useApi'
 import StatCard from '@/components/StatCard.vue'
@@ -257,7 +257,7 @@ onMounted(() => {
 // Watch for query parameter changes (e.g., browser back/forward)
 watch(
   () => route.query.type,
-  (newType) => {
+  () => {
     const stat = getInitialStatType()
     selectedStat.value = stat
     loadStat(stat)
